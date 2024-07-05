2509d55e64bdecc6914e3ba1ce973dd624856d22
yousfsalm <ywsfsalmbdallhb@gmail.com>
2024-07-05 17:45:48 +0300

    setup the program

---
+++ app.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //cout << setprecision(13); 
+  float f1 = 10.1234567891;
+  cout << f1 << "\n";
+  double dob = 10.1234567891;
+  cout << dob << "\n";
+  auto num =10;
+  cout << num << "\n";
+  int nums = 100;
+  cout << &nums;
+  string name="jfbasj";
+  char a='a';
+  cout << name <<"\n"<< a; 
+long hd="wrfghflksfadjlsfkalsdf";
+
+  return 0;
+}
